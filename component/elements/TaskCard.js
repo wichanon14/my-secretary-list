56738c259f919358501525d2eae7cf987c69f4fc
jameswich <wichanon@deepscope.com>
2021-06-28 03:05:15 +0700

    Menu Item Component & Action

--- component/elements/TaskCard.js
+++ component/elements/TaskCard.js
@@ -1,27 +1,71 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import { Menu, MenuOptions, MenuOption, MenuTrigger, renderers } from 'react-native-popup-menu';
+import { useDispatch, useSelector } from 'react-redux';
+import { UpdateTask, DeleteTask } from '../database'
+import TaskCreatedModal from './TaskCreatedModal';
 
 const TaskCard = (props) =>
 {
-    const [check,setCheck] = useState(false)
     const [iconName,setIconName] = useState("circle");
+    const [showMenu,setShowMenu] = useState(false);
+    const [showTaskCreatedModal,setShowTaskCreatedModal] = useState(false);
+    const db = useSelector(state=>state.database.connection);
+    const dispatch = useDispatch();
 
     useEffect(()=>{
-        if(check)
+        if(props.data.complete)
             setIconName("check-circle")
         else
             setIconName("circle")
-    },[check])
 
+    },[props.data.complete])
+
+    const completeTask = (data)=>
+    {
+        data.complete = (data.complete)?0:1;
+        UpdateTask(db,data,dispatch);
+    }
+
+    const deleteTask = (data)=>
+    {
+        Alert.alert(
+            "Warning","Are you sure?",
+            [
+                {
+                    text:"Yes",
+                    onPress:()=>DeleteTask(db,data,dispatch)
+                },
+                {
+                    text:"No"
+                }
+            ]
+        )
+        
+        setShowMenu(false)
+    }
 
     return(
-        <TouchableOpacity onPress={()=>setCheck(!check)}
+        <TouchableOpacity onPress={()=>completeTask(props.data)} onLongPress={()=>setShowMenu(true)}
             style={style.CardArea}>
             <Icon name={iconName} regular size={20} color="black" >
                 <View style={{width:10}}></View>
-                <Text style={style.Message}>{props.message}</Text>
+                <Text style={style.Message}>{props.data.task_name}</Text>
             </Icon>
+            <Menu opened={showMenu} onBackdropPress={()=>setShowMenu(false)} renderer={RoundedContextMenu} >
+                <MenuTrigger/>
+                <MenuOptions customStyles={optionsStyles} style={{margin:'8%'}} >
+                    <MenuOption style={{padding:'5%',borderBottomWidth:1}} onSelect={() => [setShowTaskCreatedModal(true),setShowMenu(false)]} text="Edit" />
+                    <MenuOption onSelect={() => deleteTask(props.data)}>
+                        <Text style={{ color: 'red' }}>Delete</Text>
+                    </MenuOption>
+                </MenuOptions>
+            </Menu>
+            {
+                showTaskCreatedModal && <TaskCreatedModal date={new Date(props.data.date)} 
+                    setShow={setShowTaskCreatedModal} show={showTaskCreatedModal} data={props.data}/>
+            }
         </TouchableOpacity>
     )
 }
@@ -42,5 +86,37 @@ const style = StyleSheet.create({
         flexWrap:'wrap'
     }
 })
+
+// menu option style
+const optionsStyles = {
+    optionsContainer: {
+        borderRadius:20,
+    },optionTouchable: {
+        underlayColor: 'red',
+        activeOpacity: 100,
+    }
+};
+
+
+// menu component
+const RoundedContextMenu = (props)=>
+{
+    
+    const { style, children, layouts, ...other } = props;
+    const position = computePosition(layouts);
+
+    return (
+        <View {...other} style={[roundedStyles, style, position]} >
+            {children}
+        </View>
+    );
+}
+const { computePosition } = renderers.ContextMenu;
+const roundedStyles = {
+    backgroundColor: '#f7f7f7',
+    borderRadius: 30,
+    width:'50%',
+    marginLeft:'25%'
+}
 
 export default TaskCard;
